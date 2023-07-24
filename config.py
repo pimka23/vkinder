0b432c048072e4a8b0b97bc13de1f732736c790a
Александр Пименов <pimka23@gmail.com>
2023-07-24 21:58:34 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,5 @@
+community_token = 'vk1.a.1-75SxyU-APs7Jhk9FAjeDq4Hwu8Pfq64ydG_39BnwqLyLD07g5_zduMDEvcpmbui893-FboXT4KV-1dX7Pxu0G3TU1ITjMhNhHu2t4Lq7MkBIHGqmKg0rFRC3sewk4buFGRRgPSvWZnFJj804F67y-WifRFftE1f_CqC_k2zUVBLx1BrFQy_lKV6vuHWnCtZXPtSR8bQ6naFeRJ6PdDeQ'
+access_token = 'vk1.a.YFsFD2AcPRtPS_pdWnGVerziGKROdW3yRtDCj4AwQKal11m1-zIGmAPa0twkuK8I5QDNFdxrU2_l8ZR2sosL4je1DVWMGcizMrnfAiYaj32nXwoek7_IfgFSu2D-Wmf6tD2FZvFGj5Yvc7EHvSQUGA7E5IOgq7S3CJZspr08BYyYLB87Sk1SqwAUrx-dGJuy'
+db_url_object = "postgresql://postgres:853853@localhost:5432/student_diplom"
+
+# Собрал тут токен сообщества, токен доступа приложения и ссылку на БД. К токенам буду обращаться из интерфейса и из ядра бота, к БД из дата стора.
